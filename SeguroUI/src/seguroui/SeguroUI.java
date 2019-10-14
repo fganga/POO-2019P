85e533cf3f4c57f08a03af09cbd49e1d51e4d7d1
Fredy Ganga <fredy.ganga@gmail.com>
2019-10-14 17:00:10 -0300

    Segurp UI
    Primer envío

---
+++ SeguroUI/src/seguroui/SeguroUI.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seguroui;
+
+import vista.MenuPrincipal;
+
+/**
+ *
+ * @author fredy
+ */
+public class SeguroUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Visualizamos el menù principal
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        menuPrincipal.setVisible(true);
+        
+    }
+    
+}
